1f4eb2c5da446fe71121478e40954540a412211d
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2017-06-30 04:16:12 +0100

    Added a type for spawn instructions.

--- HSCIIEngine/Types.hs
+++ HSCIIEngine/Types.hs
@@ -4,7 +4,7 @@ type Dimensions = Vec2 Int
 type Height = Int
 type Width = Int
 
-data Vec2 a = V2 a a deriving (Eq)
+data Vec2 a = V2 a a deriving (Eq, Show, Read)
 type V2F = Vec2 Float
 type X = Float
 type Y = Float
@@ -18,6 +18,12 @@ v2fzero = (V2 0 0) :: V2F
 
 v2op :: (a -> b -> c) -> Vec2 a -> Vec2 b -> Vec2 c
 v2op f (V2 a1 a2) (V2 b1 b2) = (V2 (f a1 b1) (f a2 b2))
+
+flipY :: (Num a) => Vec2 a -> Vec2 a
+flipY (V2 x y) = (V2 x (-y))
+
+flipX :: (Num a) => Vec2 a -> Vec2 a
+flipX (V2 x y) = (V2 (-x) y)
 
 instance Num a => Num (Vec2 a) where
    (+) = v2op (+)
