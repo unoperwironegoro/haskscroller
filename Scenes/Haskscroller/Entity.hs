913a794ff96e586de85a8c5edc94034a94fc1174
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2017-07-01 01:11:31 +0100

    Added properties manipulation functions.

--- Scenes/Haskscroller/Entity.hs
+++ Scenes/Haskscroller/Entity.hs
@@ -4,28 +4,34 @@ import HSCIIEngine.Types
 import HSCIIEngine.Objects
 
 import Scenes.Haskscroller.Types
+import Scenes.Haskscroller.Property
+
 import Data.Maybe
 import DataStructures.AdexMap as Adex
 
-toEntity :: Image -> V2F -> (Float, Float, Float, Float) -> [Behaviour] -> Properties
-         -> Entity
-toEntity img coords (l, t, w, h) behaviours props
-  = Ent props behaviours hitbox object
+createEntity :: Image                        -> -- Sprite
+                V2F                          -> -- Initial position
+                (Float, Float, Float, Float) -> -- Hitbox x, y, width, height
+                [Behaviour]                  -> -- Per-frame update functions
+                Properties                   -> -- Tagged Data
+                Entity
+createEntity img coords (l, t, w, h) bhvs props
+  = Ent props bhvs hbx obj
   where
-    hitbox = relativeHitbox (V2 l t) w h
-    object = (toObject img coords)
+    hbx = relativeHitbox (V2 l t) w h
+    obj = (toObject img coords)
 
 moveE :: Float -> V2F -> Entity -> Entity
-moveE scale coords (Ent ps bs h obj)
-  = Ent ps bs h (move scale obj coords)
+moveE scale coords (Ent props bhvs hbx obj)
+  = Ent props bhvs hbx (move scale obj coords)
 
 setPosE :: V2F -> Entity -> Entity
-setPosE coords (Ent ps bs h obj@(pos, _, _))
-  = Ent ps bs h (move 1 obj (coords - pos))
+setPosE coords (Ent props bhvs hbx obj@(pos, _, _))
+  = Ent props bhvs hbx (move 1 obj (coords - pos))
 
 updateE :: World -> [Action] -> (ID, Entity) -> (World, [ID])
-updateE world actions (eID, Ent _ behaviours _ _)
-  = update' behaviours world
+updateE world actions (eID, entity)
+  = update' (behaviours entity) world
   where
     update' :: [Behaviour] -> World -> (World, [ID])
     update' [] w = (w, [])
@@ -34,13 +40,16 @@ updateE world actions (eID, Ent _ behaviours _ _)
       where
         (recw, recdeads) = update' bs w'
         (w', deads) = b ie w actions
-        ie = (eID, fromJust (Adex.lookup eID w))
+        ie = (eID, fromJust (Adex.lookup eID w)) -- TODO remove?
+
+behaviours :: Entity -> [Behaviour]
+behaviours (Ent _ bhvs _ _) = bhvs
 
 posE :: Entity -> V2F
 posE (Ent _ _ _ (pos, _, _)) = pos
 
 relhitboxE :: Entity -> Hitbox
-relhitboxE (Ent _ _ hitbox _) = hitbox
+relhitboxE (Ent _ _ hbx _) = hbx
 
 hitboxE :: Entity -> Hitbox
 hitboxE entity
@@ -49,3 +58,13 @@ hitboxE entity
 moveHB :: Hitbox -> V2F -> Hitbox
 moveHB (tlbox, brbox) disp
   = (disp + tlbox, disp + brbox)
+
+------------------- Property Manipulation
+
+getVel :: Entity -> V2F -> V2F
+getVel entity defVel
+  = read (rProperty entity tagV defVel)
+
+setVel :: Entity -> V2F -> Entity
+setVel entity vel
+  = wProperty entity tagV vel
