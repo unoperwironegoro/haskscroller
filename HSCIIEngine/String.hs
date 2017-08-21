a86ffb39a3ed46d733dc64c112fa78709df12c96
Harjuno Perwironegoro <hp1515@ic.ac.uk>
2017-08-21 17:33:30 +0100

    Added some documentation and support for wrapping very long words in overflow.

--- HSCIIEngine/String.hs
+++ HSCIIEngine/String.hs
@@ -16,48 +16,63 @@ padalpha :: Width -> String -> String
 padalpha = pad alphaChar
 
 ------------------- Art
-
-artformatTransp :: Width -> String -> Char -> [String]
-artformatTransp width txt alphaCh
-  = map ((padalpha width) . (map repl))(lines txt)
+-- | Converts a \n-separated ASCII-art string into a sprite, making all
+-- | occurrences of alphaCh transparent.
+artformatTransp :: String -> Char -> Sprite
+artformatTransp txt alphaCh
+  = map ((padalpha width) . (map repl)) txtLines
   where
     repl ch | ch == alphaCh = alphaChar
             | otherwise     = ch
+    txtLines = lines txt
+    width = maximum $ map length txtLines
 
-artformat :: Width -> String -> [String]
-artformat width txt = artformatTransp width txt alphaChar
+-- | Converts a \n-separated ASCII-art string into an opaque sprite.
+artformat :: String -> Sprite
+artformat txt = artformatTransp txt alphaChar
 
 ------------------- Text handling
 
+-- | Converts a passage into an opaque sprite, with bounded by a text box of
+-- | size width, height. Excess characters are truncated.
 -- TODO handle empty newlines
-txtformat :: Width -> String -> [String]
-txtformat width txt
+txtformat :: Dimensions -> String -> Sprite
+txtformat (V2 width height) txt
   = map (padspace width) ls
   where
     paragraphs = lines txt
     ls = concatMap (overflow width) (map emptytospace wordsList)
     wordsList = (map words paragraphs)
 
---TODO personal codes
+--TODO personal codes ???
 emptytospace :: [String] -> [String]
 emptytospace [] = [""]
 emptytospace ws = ws
 
--- Assume no word with length > gwidth
 overflow :: Int -> [String] -> [String]
 overflow _ [] = []
 overflow width (w:ws)
-  = (w ++ l) : (overflow width rws)
+  = if wlen > width
+      then whead : (overflow width (wtail:ws))
+      else (w ++ l) : (overflow width rws)
   where
-    limit = width - (length w)
+    wlen = length w
+    limit = width - wlen
     (l, rws) = buildLine ws limit
+
+    (whead, wtail) = splitAt width w
 
     buildLine :: [String] -> Int -> (String, [String])
     buildLine [] _ = ([], [])
     buildLine (w:ws) lim
       = if lim' < 0
-        then ([], (w:ws))
-        else ((' ' : (w ++ l)), ls)
+          then if wl > width
+                 then (' ' : wh, (wt:ws))
+                 else ([], (w:ws))
+          else ((' ' : (w ++ l)), ls)
       where
-        lim' = lim - (1 + (length w))
+        wl = length w
+        lim' = lim - (1 + wl)
         (l, ls) = buildLine ws lim'
+
+        (wh, wt) = splitAt (lim - 1) w
